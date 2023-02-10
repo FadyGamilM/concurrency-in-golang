79855d1e101364f2fb526ca278c2b462e6563b32
fady <gamilfady605@gmail.com>
2023-02-10 14:44:52 +0200

    go keyword ..

---
+++ 02-go_routines/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// calculate time
+	start := time.Now()
+
+	go task1()
+	go task2()
+	go task3()
+	go task4()
+
+	fmt.Println("Time required to execute 4 tasks is => ", time.Since(start))
+}
+
+func task1() {
+	time.Sleep(100 * time.Millisecond)
+	fmt.Println("task 1 is done")
+}
+
+func task2() {
+	time.Sleep(200 * time.Millisecond)
+	fmt.Println("task 2 is done")
+}
+
+func task3() {
+	fmt.Println("task 3 is done")
+}
+
+func task4() {
+	time.Sleep(400 * time.Millisecond)
+	fmt.Println("task 4 is done")
+}
